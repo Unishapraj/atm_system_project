c3f2ce20cbf4ce441c289fead6665121b7c4dce2
Unisha <unishapraj7@gmail.com>
2024-11-29 12:30:42 +0545

    atm system

---
+++ atmsystem.py
@@ -0,0 +1,42 @@
+#ATM System
+'''balance = 10000
+pin = 2435
+
+print("***Insert your card***")
+pin_num = int(input("Enter your pin please:\n"))
+if pin_num == pin:
+    print("Further processing...")
+else :
+    print("Pin number is wrong!! , try again...")
+
+'''
+#ATM System
+balance = 10000
+pin = 2345
+print("**Insert your card please**")
+pin_num = int(input("Enter your pin please: \n"))
+if pin_num == pin:
+    while True:
+        print('''
+              1 - Check Balance
+              2 - Deposit
+              3 - Withdraw
+              4 - Exit
+              ''')
+        option = int (input("Choose your option \n"))
+        if option ==1:
+            print("Your current balance is {balance}")
+        elif option == 2:
+            deposit_amt = int (input("Enter your deposit amount \n"))
+            balance = balance + deposit_amt
+            print(f"Your updated balance is {balance}")
+        elif option == 3:
+            withdraw_amt= int (input("Enter your withdraw amount \n"))
+            balance = balance - withdraw_amt
+            print(f"Your updated balance is {balance} ")
+        elif option ==4:
+            break
+    print("Further Processing...")
+    
+else:
+    print("PIN Number is wrong!!, Try Again")
